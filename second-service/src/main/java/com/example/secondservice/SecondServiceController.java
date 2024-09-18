e945c1e5a5440fd746defa5e1e173e74f9a06fd4
strongmhk <kim6562166086@gmail.com>
2024-09-18 16:57:46 +0900

    :sparkles: second-service 생성 및 filter 적용

---
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,26 @@
+package com.example.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, This is a message from Second Service";
+    }
+}
