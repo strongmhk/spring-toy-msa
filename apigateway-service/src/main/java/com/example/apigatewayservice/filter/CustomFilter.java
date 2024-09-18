06af3a8cc2f59803259a409e174c6133a64f4da4
strongmhk <kim6562166086@gmail.com>
2024-09-18 18:08:44 +0900

    :sparkles: feat: CustomFilter 등록

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -0,0 +1,40 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    // PRE filter와 POST filter를 구현
+    // PRE filter: 요청이 들어오기 전에 실행되는 필터
+    // POST filter: 요청이 나가고 난 후에 실행되는 필터
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // Custom Pre Filter
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter: request id -> {}", request.getId());
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+    public static class Config {
+        // Put the configuration properties
+    }
+}
