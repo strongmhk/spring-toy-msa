9f63e8eed03cc22ada156043e5115b877544c349
strongmhk <kim6562166086@gmail.com>
2024-09-22 12:41:36 +0900

    :sparkles: feat: 로그 필터 적용

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,47 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        OrderedGatewayFilter orderedGatewayFilter = new OrderedGatewayFilter((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging PRE filter: baseMessage -> {}", config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("Logging PRE filter request id -> {}", request.getId());
+            }
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Logging POST filter: response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, OrderedGatewayFilter.HIGHEST_PRECEDENCE);
+
+        return orderedGatewayFilter;
+    }
+
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
