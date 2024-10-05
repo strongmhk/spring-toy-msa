35cbbed1ab299096ca3edf6c22457b1e80dfb0ef
strongmhk <kim6562166086@gmail.com>
2024-10-05 10:32:12 +0900

    :wrench: chore: cloud bus, amqp 적용

--- apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,8 +1,8 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
-import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+//import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+//import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -13,11 +13,11 @@ public class ApigatewayServiceApplication {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
 
-    //애플리케이션의 HTTP 요청 및 응답 데이터를 저장하고 관리하는 역할
-    // InMemoryHttpExchangeRepository 클래스를 사용하여 메모리에 저장
-    // Spring Actuator에서 HTTP 트래픽을 모니터링하기 위해 사용
-    @Bean
-    public HttpExchangeRepository httpTraceRepository() {
-        return new InMemoryHttpExchangeRepository();
-    }
+//    //애플리케이션의 HTTP 요청 및 응답 데이터를 저장하고 관리하는 역할
+//    // InMemoryHttpExchangeRepository 클래스를 사용하여 메모리에 저장
+//    // Spring Actuator에서 HTTP 트래픽을 모니터링하기 위해 사용
+//    @Bean
+//    public HttpExchangeRepository httpTraceRepository() {
+//        return new InMemoryHttpExchangeRepository();
+//    }
 }
